e4e12a7f751141315791c12ea0aa83c759268a31
Douglas Luman <dluman@douglasjluman.com>
2020-11-14 03:46:34 +0000

    There were some nice ones tho

--- King.py
+++ King.py
@@ -1,6 +1,27 @@
+from Vader import Sentiment
+
 class Bio:
 
     def __init__(self, title, text):
         self.title = title
         self.text = text
-        self.meanness = 0
+        self.meanness = self.assess_meanness()
+
+    def assess_meanness(self):
+        meanness = 1
+        sentiment = Sentiment.classify(
+            self.text
+        )["compound"]
+        if sentiment < -.5:
+            meanness = 4
+        elif sentiment < -.25:
+            meanness = 3
+        elif sentiment < .25:
+            meanness = 2
+        elif sentiment > .25:
+            meanness = 1
+        print(sentiment) 
+        return meanness
+
+    def generate_sentence(self):
+        pass
--- main.py
+++ main.py
@@ -3,9 +3,4 @@ from King import Bio
 from Vader import Sentiment
 
 entry = Entry()
-
-#print(f"{entry.title} {entry.text}")
-
 king = Bio(entry.title, entry.text)
-
-print(Sentiment.classify(entry.text))
